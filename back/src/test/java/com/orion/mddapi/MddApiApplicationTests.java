bf82786dbfb5915acb64694e8ce3870a965e5c12
Faten <faten.mars@mars-tech.dev>
2026-07-22 11:13:16 +0200

    init back

---
+++ back/src/test/java/com/orion/mddapi/MddApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orion.mddapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MddApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
