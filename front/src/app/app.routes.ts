ebac7a12e11efe6c5a448102b89a1ee831b1b971
Faten <faten.mars@mars-tech.dev>
2026-08-13 16:53:30 +0200

    WIP la liste est affichée côté front

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { FeedComponent } from './pages/feed/feed.component';
 import { ArticleDetailComponent } from './pages/article-detail/article-detail.component';
 import { CreateArticleComponent } from './pages/create-article/create-article.component';
+import { ThemesComponent } from './pages/themes/themes.component';
 
 export const routes: Routes = [
   { path: '', component: FeedComponent },
   { path: 'articles/new', component: CreateArticleComponent },
   { path: 'articles/:id', component: ArticleDetailComponent },
+  { path: 'themes', component: ThemesComponent },
 ];
