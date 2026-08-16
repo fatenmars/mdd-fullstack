41164532dc41178070d1ca5717651d4a7171b73d
Faten <faten.mars@mars-tech.dev>
2026-08-16 13:56:16 +0200

    WIP: register - back POST /auth/register (unicite + hachage)

---
+++ back/src/main/java/com/orion/mddapi/controllers/AuthController.java
@@ -0,0 +1,25 @@
+package com.orion.mddapi.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.orion.mddapi.dto.RegisterRequest;
+import com.orion.mddapi.services.AuthService;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public void register(@Valid @RequestBody RegisterRequest request) {
+        this.authService.register(request);
+    }
+}
