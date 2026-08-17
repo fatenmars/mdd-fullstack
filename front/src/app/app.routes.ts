5ca81e4dd6b368ea264a3e266b3bceac6450a507
Faten <faten.mars@mars-tech.dev>
2026-08-17 16:09:02 +0200

    WIP: secure-api front - inte^Cepteur + garde + gestion 401

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -6,13 +6,22 @@ import { ThemesComponent } from './pages/themes/themes.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './pages/login/login.component';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
-  { path: '', component: FeedComponent },
-  { path: 'articles/new', component: CreateArticleComponent },
-  { path: 'articles/:id', component: ArticleDetailComponent },
-  { path: 'themes', component: ThemesComponent },
-  { path: 'profile', component: ProfileComponent },
+  { path: '', component: FeedComponent, canActivate: [authGuard] },
+  {
+    path: 'articles/new',
+    component: CreateArticleComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: 'articles/:id',
+    component: ArticleDetailComponent,
+    canActivate: [authGuard],
+  },
+  { path: 'themes', component: ThemesComponent, canActivate: [authGuard] },
+  { path: 'profile', component: ProfileComponent, canActivate: [authGuard] },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
 ];
