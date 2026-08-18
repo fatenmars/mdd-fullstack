fd78f8b8cde69f8eec6167deb55524eb4b29a1c1
Faten <faten.mars@mars-tech.dev>
2026-08-18 20:16:16 +0200

    WIP: style-auth - login desktop (auth-layout partage, formulaire)

--- front/src/app/pages/login/login.component.ts
+++ front/src/app/pages/login/login.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthLayoutComponent } from '../../components/auth-layout/auth-layout.component';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule],
+  imports: [FormsModule, AuthLayoutComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
