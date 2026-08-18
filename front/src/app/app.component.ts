ef4498ba38b575cf4ee51a794f088e42cd9f6d32
Faten Mars <97958070+fatenmars@users.noreply.github.com>
2026-08-18 18:52:58 +0200

    43 feature/navbar (#46)

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { FeedComponent } from './pages/feed/feed.component';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterModule],
+  imports: [RouterModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
